47303f45973d281f6f42d2b324c34541c161e53d
anouarbousraou <70587352+anouarbousraou@users.noreply.github.com>
2021-03-07 22:19:44 +0100

    Add files via upload

--- grafiek.py
+++ grafiek.py
@@ -1,6 +1,8 @@
 import matplotlib.pyplot as plt
 import csv
 import matplotlib.patches as mpatches
+import numpy as np
+
 
 
 y = []
@@ -14,13 +16,15 @@ with open ("export_ipt.csv", "r") as data:
         x.append(float(row.get("x")))
         y.append(float(row.get("y")))
 
+with plt.style.context('dark_background'):
+    ax = plt.subplot()
+    legenda = mpatches.Patch(color='khaki', label='Portfolio of Gold fund & ETF on the MSCI ChinaIndex')
+    plt.legend(handles=[legenda])
+    ret_plt = ax.scatter(x, y, color='khaki')
 
-ax = plt.subplot()
-legenda = mpatches.Patch(color='r', label='Portfolio of Gold fund & ETF on the MSCI ChinaIndex')
-plt.legend(handles=[legenda])
-ret_plt = ax.scatter(x, y, color='r')
-plt.plot(x,y,color='b')
-plt.xlabel("Standard Deviation in %")
-plt.ylabel("Return in %")
+    plt.plot(x,y,color='y')
+  
+    plt.xlabel("Standard Deviation in %")
+    plt.ylabel("Return in %")
 
 plt.savefig("test.png")
